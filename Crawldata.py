e92ba166dd99b81997c4ef88698e892d33ca3e92
huy0301 <134368426+huy0301@users.noreply.github.com>
2024-11-21 00:57:53 +0700

    Crawldata.py

--- Crawldata.py
+++ Crawldata.py
@@ -16,20 +16,20 @@ service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 
 # URL của trang web cần crawl
-url = "https://www.nhatot.com/thue-bat-dong-san-tp-ho-chi-minh"
+url = "https://www.nhatot.com/mua-ban-bat-dong-san"
 
 # Mở trang web
 driver.get(url)
 
 # Đợi một chút để trang web tải xong
-time.sleep(120)
+time.sleep(5)
 
 # Danh sách để lưu dữ liệu
 data = []
 
 # Bộ đếm số trang và giới hạn số trang
 page_count = 1
-max_pages = 30  # Số trang tối đa bạn muốn thu thập dữ liệu
+max_pages = 10  # Số trang tối đa bạn muốn thu thập dữ liệu
 
 while page_count <= max_pages:
     # Tìm tất cả các thẻ chứa thông tin bất động sản
@@ -39,6 +39,19 @@ while page_count <= max_pages:
         title = listing.find_element(By.CLASS_NAME, 'a15fd2pn').text.strip() if listing.find_elements(By.CLASS_NAME, 'a15fd2pn') else 'Không có'
         price = listing.find_element(By.CSS_SELECTOR, 'span[style="color: rgb(229, 25, 59); cursor: inherit; font-size: 16px;"]').text.strip() if listing.find_elements(By.CSS_SELECTOR, 'span[style="color: rgb(229, 25, 59); cursor: inherit; font-size: 16px;"]') else 'Không có'
         
+        # Lấy kiểu bất động sản
+        details = listing.find_element(By.CLASS_NAME, 'bwq0cbs.tle2ik0').text.strip() if listing.find_elements(By.CLASS_NAME, 'bwq0cbs.tle2ik0') else 'Không có'
+        
+        # Phân loại kiểu bất động sản
+        if "chung cư" in details.lower() or "căn hộ" in details.lower():
+            real_estate_type = "Nhà thương mại"
+        elif "nhà" in details.lower():
+            real_estate_type = "Nhà ở"
+        elif "đất" in details.lower():
+            real_estate_type = "Công nghiệp"
+        else:
+            real_estate_type = "Không xác định"
+
         # Lấy tất cả các thẻ span có class 'bfe6oav'
         area_spans = listing.find_elements(By.CLASS_NAME, 'bfe6oav')
         
@@ -55,7 +68,9 @@ while page_count <= max_pages:
             'Giá': price,
             'Giá trên 1 mét vuông': price_per_m2,
             'Tổng diện tích': total_area,
-            'Địa chỉ (Tên thành phố)': address_info
+            'Địa chỉ (Tên thành phố)': address_info,
+            'Kiểu bất động sản': details,
+            'Phân loại': real_estate_type
         })
 
     print(f"Đã thu thập dữ liệu từ trang {page_count}.")
@@ -83,9 +98,9 @@ while page_count <= max_pages:
 df = pd.DataFrame(data)
 
 # Lưu DataFrame vào file CSV
-df.to_csv('bat_dong_san1.csv', index=False, encoding='utf-8-sig')
+df.to_csv('bat_dong_san_Mua_ban.csv', index=False, encoding='utf-8-sig')
 # Hoặc lưu vào file Excel
-df.to_excel('bat_dong_san1.xlsx', index=False, engine='openpyxl')
+df.to_excel('bat_dong_san_Mua_ban.xlsx', index=False, engine='openpyxl')
 
 print("Dữ liệu đã được lưu vào file bat_dong_san.csv và bat_dong_san.xlsx")
 
